35f84a05c0b8d22722c864c317bcd2be08bf7583
Eric Gao <ejgao@ucdavis.edu>
2024-07-24 11:24:25 -0500

    fix demo

--- demo.py
+++ demo.py
@@ -74,177 +74,99 @@ def get_model(dir):
 
     return data_parameters, torch.load(dir+'model.pt')
 
-if "__main__" == __name__:
-    tot_perf_lag = {}
+if '__main__' == __name__:
+    tot_perf_lag={}
     tot_perf_no_lag = {}
-
-    num_nodes = params["d"]
-
-    trials = params["trials"]
-    print_params = False
+    
+    num_nodes = params['d']
+    
+    trials = params['trials']
     results = []
-    for trial in range(args.current_start, trials):
-        cur_seed = trial + seed
-        print("current seed:{}".format(cur_seed))
-        COUNT = cur_seed
+    print_params=False
+
+    for trial in range(args.current_start,trials):
+        cur_seed=trial+seed
+        print('current seed:{}'.format(cur_seed))
+        COUNT=cur_seed
         if not COUNT:
             print_params = True
-        if args.data_type == "var":
-            data_name = "num_nodes_{}/true_lag_{}/noise_scale_{}/seed_{}/".format(
-                params["d"], params["true_lag"], params["noise_scale"], cur_seed
-            )
-
-        elif args.data_type == "lorenz":
-            data_name = "num_nodes_{}/F_{}/seed_{}/".format(
-                params["d"], params["f_lorenz"], cur_seed
-            )
-        elif args.data_type == "fmri":
-            data_name = "num_nodes_{}/subject_{}/seed_{}/".format(
-                params["d"], args.f_subject, cur_seed
-            )
-        elif args.data_type == "dream3":
-            dream3_data_list = ["EColi1", "EColi2", "Yeast1", "Yeast2", "Yeast3"]
+        if args.data_type == 'var':
+            data_name = 'num_nodes_{}/true_lag_{}/noise_scale_{}/seed_{}/'.format(params['d'],params['true_lag'],params['noise_scale'],cur_seed)
+            
+        elif args.data_type == 'lorenz':
+            data_name = 'num_nodes_{}/F_{}/seed_{}/'.format(params['d'],params['f_lorenz'],cur_seed)
+        elif args.data_type == 'fmri':
+            data_name = 'num_nodes_{}/subject_{}/seed_{}/'.format(params['d'],args.f_subject,cur_seed)
+        elif args.data_type == 'dream3':
+            dream3_data_list = ['EColi1','EColi2','Yeast1', 'Yeast2', 'Yeast3']
             dream3_using_data = dream3_data_list[args.dream3_subject]
-            data_name = "num_nodes_{}/{}/seed_{}/".format(
-                params["d"], dream3_using_data, cur_seed
-            )
+            data_name = 'num_nodes_{}/{}/seed_{}/'.format(params['d'],dream3_using_data,cur_seed)
 
-        if not os.path.exists(data_dir + data_name):
-            os.makedirs(data_dir + data_name)
+        if not os.path.exists(data_dir+data_name):
+                os.makedirs(data_dir+data_name)
 
-        try:  # loading data
-            #1/0 
-            x = np.load(data_dir + data_name + "_x.npy")
-            print(x.shape)
-            x_eval = np.load(data_dir + data_name + "_x_eval.npy")
-            gc = np.load(data_dir + data_name + "_gc.npy")
+        try:
+            x = np.load(data_dir+data_name+'_x.npy')
+            x_eval = np.load(data_dir+data_name+'_x_eval.npy')
+            gc = np.load(data_dir+data_name+'_gc.npy')
             print("data loaded...")
-        except:  # generating data
-
-            if args.data_type == "var":
+        except:
+            
+            if args.data_type == 'var':
                 print("generating data from scratch...")
-                # x, x_eval, gc, beta = var_stable(
-                #     d=num_nodes,
-                #     t=args.var_t,
-                #     t_eval=args.var_t_eval,
-                #     lag=params["true_lag"],
-                #     sd=params["noise_scale"],
-                #     seed=cur_seed,
-                # ) 
-                # change generate data to a function which calls var_stable
-                x, x_eval, y, a_mat = custom_var_function(n = 1000, p = num_nodes, seed = cur_seed)
-                #print(x.shape)
-                #print(beta.shape)
-                #np.save(data_dir + data_name + "_x.npy", x)
-                #np.save(data_dir + data_name + "_x_eval.npy", x_eval)
-                #np.save(data_dir + data_name + "_gc.npy", gc)
-                #np.save(data_dir + data_name + "_beta.npy", beta)  # save the beta
-
-            elif args.data_type == "fmri":
-                print("-----fmri----")
-                x, x_eval, gc = fmri_net_sim(
-                    d=num_nodes,
-                    subject=args.f_subject,
-                    t=args.f_t,
-                    t_eval=args.f_t_eval,
-                )
-            elif args.data_type == "lorenz":
+                x, x_eval, gc = var_stable(d=num_nodes, t=args.var_t, t_eval=args.var_t_eval, lag=params['true_lag'], sd=params['noise_scale'],seed=cur_seed)
+                np.save(data_dir+data_name+'_x.npy',x)
+                np.save(data_dir+data_name+'_x_eval.npy',x_eval)
+                np.save(data_dir+data_name+'_gc.npy',gc)
+            elif args.data_type == 'fmri':
+                print('-----fmri----')
+                x, x_eval, gc = fmri_net_sim(d=num_nodes, subject=args.f_subject, t=args.f_t, t_eval=args.f_t_eval)
+            elif args.data_type == 'lorenz':
                 print("generating data from scratch...")
-                x, x_eval, gc = lorenz_96(
-                    d=num_nodes,
-                    t=args.lorenz_t,
-                    t_eval=args.lorenz_t_eval,
-                    f=params["f_lorenz"],
-                    seed=cur_seed,
-                )
-                np.save(data_dir + data_name + "_x.npy", x)
-                np.save(data_dir + data_name + "_x_eval.npy", x_eval)
-                np.save(data_dir + data_name + "_gc.npy", gc)
-            elif args.data_type == "dream3":
-                print("dream3-------")
-                x, x_eval, gc = dream3_trajectories(
-                    d=num_nodes, subject=args.dream3_subject
-                )
+                x, x_eval, gc = lorenz_96(d=num_nodes, t=args.lorenz_t, t_eval=args.lorenz_t_eval, f=params['f_lorenz'], seed=cur_seed)
+                np.save(data_dir+data_name+'_x.npy',x)
+                np.save(data_dir+data_name+'_x_eval.npy',x_eval)
+                np.save(data_dir+data_name+'_gc.npy',gc)
+            elif args.data_type == 'dream3':
+                print('dream3-------')
+                x, x_eval, gc = dream3_trajectories(d=num_nodes, subject=args.dream3_subject)
+            
+            
+
+
+        
+        
 
         tm = time.time()
+        
+
 
         if args.jaco_param:
-            # training
-            model, it, a, b, c, best_loss_r, mean_pred_loss_r, eval_loss_r = (
-                JRNGC.from_train(
-                    max_iter=params["max_iter"],
-                    d=params["d"],
-                    lag=params["lag"],
-                    layers=params["layers"],
-                    hidden=params["hidden"],
-                    dropout=params["dropout"],
-                    jacobian_lam=args.jaco_param,
-                    struct_loss_choice=params["struct_loss_choice"],
-                    JFn=params["JFn"],
-                    x=x,
-                    x_eval=x_eval,
-                    y = y, 
-                    lr=params["lr"],
-                    seed=cur_seed,
-                    device=device,
-                    verbose=params["verbose"],
-                    relu=args.jrngc_relu,
-                )
-            )
+            model, it, a, b, c,best_loss_r, mean_pred_loss_r, eval_loss_r = JRNGC.from_train(max_iter=params['max_iter'], d=params['d'], lag=params['lag'], layers=params['layers'], hidden=params['hidden'], dropout=params['dropout'],  jacobian_lam=args.jaco_param,struct_loss_choice=params['struct_loss_choice'],JFn =params['JFn'], x=x, x_eval=x_eval, lr=params['lr'], seed=cur_seed, device=device, verbose=params['verbose'],relu = args.jrngc_relu)
         else:
-            model, it, a, b, c, best_loss_r, mean_pred_loss_r, eval_loss_r = (
-                JRNGC.from_train(
-                    max_iter=params["max_iter"],
-                    d=params["d"],
-                    lag=params["lag"],
-                    layers=params["layers"],
-                    hidden=params["hidden"],
-                    dropout=params["dropout"],
-                    jacobian_lam=params["jacobian_lam"],
-                    struct_loss_choice=params["struct_loss_choice"],
-                    JFn=params["JFn"],
-                    x=x,
-                    x_eval=x_eval,
-                    y = y,
-                    lr=params["lr"],
-                    seed=cur_seed,
-                    device=device,
-                    verbose=params["verbose"],
-                    relu=args.jrngc_relu,
-                )
-            )
-        key_metric = ["causal with lag use jaco", "causal no lag use jaco"]
+            model, it, a, b, c,best_loss_r, mean_pred_loss_r, eval_loss_r = JRNGC.from_train(max_iter=params['max_iter'], d=params['d'], lag=params['lag'], layers=params['layers'], hidden=params['hidden'], dropout=params['dropout'], jacobian_lam=params['jacobian_lam'],struct_loss_choice=params['struct_loss_choice'],JFn =params['JFn'], x=x, x_eval=x_eval, lr=params['lr'], seed=cur_seed, device=device, verbose=params['verbose'],relu=args.jrngc_relu)
+        key_metric = ['causal with lag use jaco', 'causal no lag use jaco']
         end_time = time.time()
         if print_params:
             num_params = count_num_param(model)
-            with open(
-                "./result/{}/model_params_result_{}_{}.txt".format(
-                    args.model_type, args.data_type, num_nodes
-                ),
-                "a",
-            ) as f:
-
-                f.write("it is running {} Nodes {}\n".format(args.data_type, num_nodes))
-                f.write(f"the seed is {seed}\n")
-                if args.model_type == "JRNGC":
-                    f.write(
-                        "the JRNGC {}  params:{}\n".format(
-                            params["struct_loss_choice"], num_params
-                        )
-                    )
-
-                f.write("It is using the yaml file:{}\n".format(args.yaml_dir))
-                f.write("========================\n")
-
+            with open('./result/{}/model_params_result_{}_{}.txt'.format(args.model_type,args.data_type,num_nodes),'a') as f:
+                
+                f.write('it is running {} Nodes {}\n'.format(args.data_type,num_nodes))
+                f.write(f'the seed is {seed}\n')
+                if args.model_type=='JRNGC':
+                    f.write('the JRNGC {}  params:{}\n'.format(params['struct_loss_choice'],num_params))
+                
+                
+                f.write('It is using the yaml file:{}\n'.format(args.yaml_dir))
+                f.write('========================\n')
+       
+        
         ret = model.get_gc_metrics(x, gc)
-
+        
         ret = [ret[key_metric[0]], ret[key_metric[1]]]
         key_i = 0
-        result_filename = "./result/{}/data_{}/seed_{}.txt".format(
-            args.model_type, args.data_type, cur_seed
-        )
+        result_filename = './result/{}/data_{}/seed_{}.txt'.format(args.model_type,args.data_type,cur_seed)
         os.makedirs(os.path.dirname(result_filename), exist_ok=True)
-        # results = []
         for it in ret:
             # print()
             # print(f"f1: {it['f1']:.3f}, f1_eps: {it['f1_eps']:.3f}")
@@ -288,16 +210,14 @@ if "__main__" == __name__:
     print(means)
     print("SDS: ")
     print(sds)
-
-    #     #for result in results:
-    #         #print(result)
+            
 
     #         with open(result_filename, 'a') as f:
     #             f.write("----Tt is running seed : {}----------------------------\n".format(cur_seed))
     #             f.write("----Tt is running {}----------------------------\n".format(key_metric[key_i]))
 
     #             f.write('used_time:{:.6f}\n'.format(end_time - tm))
-
+               
     #             if args.model_type == 'JRNGC':
     #                 f.write('the struct_loss is {}\n'.format(params['struct_loss_choice']))
     #             f.write('it is using the yaml file:{} \n'.format(using_yaml_file))
@@ -327,27 +247,29 @@ if "__main__" == __name__:
 
     #         key_i += 1
 
-    # #print(results)
+            
     # for key,value in tot_perf_lag.items():
-
+       
     #     perf=np.array(value["value"])
     #     tot_perf_lag[key]['mean']=float(np.mean(perf))
     #     tot_perf_lag[key]['std']=float(np.std(perf))
     # for key,value in tot_perf_no_lag.items():
-
+       
     #     perf=np.array(value["value"])
     #     tot_perf_no_lag[key]['mean']=float(np.mean(perf))
     #     tot_perf_no_lag[key]['std']=float(np.std(perf))
     # f_dir = './result/{}/data_{}/'.format(args.model_type,args.data_type)
-
+    
     # json_filename = f_dir+data_name[:-3]
     # os.makedirs(os.path.dirname(json_filename), exist_ok=True)
     # yaml_filename = os.path.splitext(os.path.basename(args.yaml_dir))[0]
 
-    # with open(json_filename+"_"+key_metric[0]+"_"+yaml_filename+"_"+'yaml'+".json",'w') as f:
-    #     json.dump(tot_perf_lag,f)
-
-    # with open(json_filename+"_"+key_metric[1]+"_"+yaml_filename+"_"+'yaml'+".json",'w') as f:
-    #     json.dump(tot_perf_no_lag,f)
-
+    # with open(json_filename+"_"+key_metric[0]+"_"+yaml_filename+"_"+'yaml'+".json",'w') as f:    
+    #     json.dump(tot_perf_lag,f)  
+       
+    # with open(json_filename+"_"+key_metric[1]+"_"+yaml_filename+"_"+'yaml'+".json",'w') as f:    
+    #     json.dump(tot_perf_no_lag,f)  
+        
+    
+        
     
